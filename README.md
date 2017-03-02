a15eff8b305e0504a89d339ee4fae7bc27a4658d
ricky <rqcao@outlook.com>
2017-03-02 10:28:52 +0800

    add taobao page

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 一些页面小练习
 
 - [魔兽页面](http://copypage.rickyer.com/wow/index.html)
+
+- [淘宝页面](http://copypage.rickyer.com/tb_new/taobao/index.html)
---
+++ taobao/tb.js
@@ -0,0 +1,263 @@
+function change2baby(){
+	var baby=document.getElementById('baby-txt');
+	baby.style.color="white";
+	baby.style.background="#FF4400";
+	baby.style.fontWeight="700";
+
+	var tmail=document.getElementById('tmail-txt');
+	tmail.style.color="black";
+	tmail.style.background="none";
+	tmail.style.fontWeight="400";
+
+	document.getElementById('search-input').style.borderColor="#FF4400";
+
+	var store=document.getElementById('store-txt');
+	store.style.color="black";
+	store.style.background="none";
+	store.style.fontWeight="400";	
+
+	document.getElementById('lyu').style.color="#FF4400";
+	document.getElementById('pad').style.color="#FF4400";
+	document.getElementById('my').style.color="#FF4400";
+}
+
+function change2tmail(){
+	var baby=document.getElementById('baby-txt');
+	baby.style.color="black";
+	baby.style.background="none";
+	baby.style.fontWeight="400";
+
+	var tmail=document.getElementById('tmail-txt');
+	tmail.style.color="white";
+	tmail.style.background="#C40000";
+	tmail.style.fontWeight="700";
+
+	document.getElementById('search-input').style.borderColor="#C40000";
+
+	var store=document.getElementById('store-txt');
+	store.style.color="black";
+	store.style.background="none";
+	store.style.fontWeight="400";	
+
+	document.getElementById('lyu').style.color="#C40000";
+	document.getElementById('pad').style.color="#C40000";
+	document.getElementById('my').style.color="#C40000";
+}
+
+function change2store(){
+	var baby=document.getElementById('baby-txt');
+	baby.style.color="black";
+	baby.style.background="none";
+	baby.style.fontWeight="400";
+
+	var tmail=document.getElementById('tmail-txt');
+	tmail.style.color="black";
+	tmail.style.background="none";
+	tmail.style.fontWeight="400";
+
+	document.getElementById('search-input').style.borderColor="#FF4400";
+
+	var store=document.getElementById('store-txt');
+	store.style.color="white";
+	store.style.background="#FF4400";
+	store.style.fontWeight="700";	
+
+	document.getElementById('lyu').style.color="#FF4400";
+	document.getElementById('pad').style.color="#FF4400";
+	document.getElementById('my').style.color="#FF4400";
+}
+
+function camera2orange(){
+	document.getElementById("icon-camera").src="image/icon1/camera-orange.png";
+}
+
+function camera2hui(){
+	document.getElementById("icon-camera").src="image/icon1/camera.png";
+}
+
+function delete_ewm(){
+	document.getElementById("ewm-cha").style.opacity="0";
+	document.getElementById("right").style.opacity="0";
+}
+
+function change2notice(){
+	var notice=document.getElementById('actop-notice');
+	notice.style.background="white";
+	notice.style.fontWeight="700";
+	notice.style.borderBottom="none";
+
+	var rule=document.getElementById('actop-rule');
+	rule.style.background="none";
+	rule.style.fontWeight="400";
+	rule.style.borderBottom="1px solid rgb(245,245,245)";
+
+	var bbs=document.getElementById('actop-bbs');
+	bbs.style.background="none";
+	bbs.style.fontWeight="400";
+	bbs.style.borderBottom="1px solid rgb(245,245,245)";
+
+	var safety=document.getElementById('actop-safety');
+	safety.style.background="none";
+	safety.style.fontWeight="400";
+	safety.style.borderBottom="1px solid rgb(245,245,245)";
+
+	var benefit=document.getElementById('actop-benefit');
+	benefit.style.background="none";
+	benefit.style.fontWeight="400";
+	benefit.style.borderBottom="1px solid rgb(245,245,245)";
+
+	document.getElementById('actop-cont-1').innerHTML="纽交所将在水立方为双11举行远程开市敲钟仪式";
+	document.getElementById('actop-cont-2').innerHTML="天猫iPhone6s再次降价 若买到假货退全款赔4倍";
+}
+
+function change2rule(){
+	var notice=document.getElementById('actop-notice');
+	notice.style.background="none";
+	notice.style.fontWeight="400";
+	notice.style.borderBottom="1px solid rgb(245,245,245)";
+
+	var rule=document.getElementById('actop-rule');
+	rule.style.background="white";
+	rule.style.fontWeight="700";
+	rule.style.borderBottom="0";
+
+	var bbs=document.getElementById('actop-bbs');
+	bbs.style.background="none";
+	bbs.style.fontWeight="400";
+	bbs.style.borderBottom="1px solid rgb(245,245,245)";
+
+	var safety=document.getElementById('actop-safety');
+	safety.style.background="none";
+	safety.style.fontWeight="400";
+	safety.style.borderBottom="1px solid rgb(245,245,245)";
+
+	var benefit=document.getElementById('actop-benefit');
+	benefit.style.background="none";
+	benefit.style.fontWeight="400";
+	benefit.style.borderBottom="1px solid rgb(245,245,245)";
+
+	document.getElementById('actop-cont-1').innerHTML="10月新规发布汇总 广告法禁用语大全";
+	document.getElementById('actop-cont-2').innerHTML="邮费争议问题汇总 炒信规则调整意见反馈";
+}
+
+function change2bbs(){
+	var notice=document.getElementById('actop-notice');
+	notice.style.background="none";
+	notice.style.fontWeight="400";
+	notice.style.borderBottom="1px solid rgb(245,245,245)";
+
+	var rule=document.getElementById('actop-rule');
+	rule.style.background="none";
+	rule.style.fontWeight="400";
+	rule.style.borderBottom="0";
+
+	var bbs=document.getElementById('actop-bbs');
+	bbs.style.background="white";
+	bbs.style.fontWeight="700";
+	bbs.style.borderBottom="1px solid rgb(245,245,245)";
+
+	var safety=document.getElementById('actop-safety');
+	safety.style.background="none";
+	safety.style.fontWeight="400";
+	safety.style.borderBottom="1px solid rgb(245,245,245)";
+
+	var benefit=document.getElementById('actop-benefit');
+	benefit.style.background="none";
+	benefit.style.fontWeight="400";
+	benefit.style.borderBottom="1px solid rgb(245,245,245)";
+
+	document.getElementById('actop-cont-1').innerHTML="阿里建商家全链路服务系 个性化的消费时代来临";
+	document.getElementById('actop-cont-2').innerHTML="跨境贸易是中小企业机遇 美妆行业虚假信息管控";
+}
+
+function change2safety(){
+	var notice=document.getElementById('actop-notice');
+	notice.style.background="none";
+	notice.style.fontWeight="400";
+	notice.style.borderBottom="1px solid rgb(245,245,245)";
+
+	var rule=document.getElementById('actop-rule');
+	rule.style.background="none";
+	rule.style.fontWeight="400";
+	rule.style.borderBottom="1px solid rgb(245,245,245)";
+
+	var bbs=document.getElementById('actop-bbs');
+	bbs.style.background="none";
+	bbs.style.fontWeight="400";
+	bbs.style.borderBottom="1px solid rgb(245,245,245)";
+
+	var safety=document.getElementById('actop-safety');
+	safety.style.background="white";
+	safety.style.fontWeight="700";
+	safety.style.borderBottom="0";
+
+	var benefit=document.getElementById('actop-benefit');
+	benefit.style.background="none";
+	benefit.style.fontWeight="400";
+	benefit.style.borderBottom="1px solid rgb(245,245,245)";
+
+	document.getElementById('actop-cont-1').innerHTML="接次文件，毁了一家店 警企联手打击网络犯罪";
+	document.getElementById('actop-cont-2').innerHTML="千牛安全助手上线了 益行侠-违规商品捉妖";
+}
+
+function change2benefit(){
+	var notice=document.getElementById('actop-notice');
+	notice.style.background="none";
+	notice.style.fontWeight="400";
+	notice.style.borderBottom="1px solid rgb(245,245,245)";
+
+	var rule=document.getElementById('actop-rule');
+	rule.style.background="none";
+	rule.style.fontWeight="400";
+	rule.style.borderBottom="1px solid rgb(245,245,245)";
+
+	var bbs=document.getElementById('actop-bbs');
+	bbs.style.background="none";
+	bbs.style.fontWeight="400";
+	bbs.style.borderBottom="1px solid rgb(245,245,245)";
+
+	var safety=document.getElementById('actop-safety');
+	safety.style.background="none";
+	safety.style.fontWeight="400";
+	safety.style.borderBottom="1px solid rgb(245,245,245)";
+
+	var benefit=document.getElementById('actop-benefit');
+	benefit.style.background="white";
+	benefit.style.fontWeight="700";
+	benefit.style.borderBottom="1px solid rgb(245,245,245)";
+
+	document.getElementById('actop-cont-1').innerHTML="公益宝贝捐款规则调整";
+	document.getElementById('actop-cont-2').innerHTML="公益宝贝卖家发票索取";
+}
+
+/*
+var i = 0;
+var time=1000;//5秒更改
+
+function mothed(){
+	if(i==0) document.getElementById("hdl-cont")..innerHTML="MUJU风格家居，一种生活的哲学";
+	else if(i==2) document.getElementById("hdl-cont").innerHTML="维密史上价值13亿的19件";
+	else if(i==1) document.getElementById("hdl-cont").innerHTML="利用3分钟零散时间轻松减肥！";
+
+	i=i+1;
+	if(i==3){i=0;}
+}
+setInterval(mothed,time); 
+*/
+
+function change2bigad0(){
+	document.getElementById('big-ad').src="image/bigad0.jpg"
+}
+function change2bigad1(){
+	document.getElementById('big-ad').src="image/bigad1.jpg"
+}
+function change2bigad2(){
+	document.getElementById('big-ad').src="image/bigad2.jpg"
+}
+function change2bigad3(){
+	document.getElementById('big-ad').src="image/bigad3.jpg"
+}
+function change2bigad4(){
+	document.getElementById('big-ad').src="image/bigad4.jpg"
+}
+
